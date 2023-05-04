84c1e4fbe74e93455cb6be4e3e6816978e1db3dc
Jan Macheta <jan.macheta@embe.tech>
2023-05-04 15:29:21 +0200

    release candidate

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,44 @@
 #include "serial_connection.hpp"
 
-#include <termcolor/termcolor.hpp>
+#include <argparse/argparse.hpp>
+#include <fmt/color.h>
+#include <fmt/ostream.h>
 
 #include <algorithm>
-#include <charconv>
 #include <chrono>
 #include <cstdlib>
-#include <iostream>
+#include <cstring>
 #include <random>
 #include <thread>
 #include <vector>
 
+#ifndef APP_VERSION
+#    define APP_VERSION "0.0.0"
+#endif
 
-void on_app_finished() {
-    std::cout << termcolor::yellow << "============ USART stress test application end ============" << termcolor::reset << std::endl;
-}
 
 using namespace std::literals::string_view_literals;
 using namespace std::literals::chrono_literals;
+using namespace std::literals::string_literals;
+constexpr auto response_timeout         = 1s;
+constexpr auto response_expected_length = 4;
 
-constexpr auto response_timeout         = 2s;
-constexpr auto response_expected_length = 2;
+
+bool verbose = false;
 
 
-void display_help() {
-    std::cout << "usage: usart_stress_test <serial_port> <baudrate>\n";
-}
-
-unsigned parse_baudrate(std::string_view str) {
-    unsigned baudrate;
-    auto [p, ec] = std::from_chars(str.begin(), str.end(), baudrate);
-
-    return (std::errc() == ec) ? baudrate : 0;
+void on_app_finished() {
+    fmt::print(fg(fmt::color::lemon_chiffon), "======= USART stress test application end =======");
 }
 
 
-void perform_test(SerialConnection& c, std::size_t bytes_to_send) {
+int fail_with_reason(std::string_view reason) {
+    fmt::print(fg(fmt::color::aqua), "Test finished with result: ");
+    fmt::print(fg(fmt::color::indian_red), "FAIL - {}\n", reason);
+    return 0;
+}
+
+int perform_test(SerialConnection& c, std::size_t bytes_to_send) {
     std::random_device                          dev;
     std::mt19937                                rng(dev());
     std::uniform_int_distribution<std::uint8_t> dist(1);
@@ -44,64 +47,127 @@ void perform_test(SerialConnection& c, std::size_t bytes_to_send) {
 
     std::ranges::generate(to_send, gen);
 
-    auto checksum = std::accumulate(to_send.begin(), to_send.end(), std::uintmax_t(0));
-
-    std::cout << "to_send: ";
-    for (auto c : to_send) {
-        std::cout << std::hex << static_cast<int>(c) << ',';
+    auto checksum = std::accumulate(to_send.begin(), to_send.end(), std::uint32_t(0));
+    if (verbose) {
+        fmt::print("\tSending {} bytes. Expected checksum {:#x}", bytes_to_send, checksum);
     }
-    std::cout << '\n';
-    std::cout << "checksum: " << checksum << "\n";
-
     c.clear();
     c.write({reinterpret_cast<std::byte const*>(to_send.data()), to_send.size()});
 
-    auto wait_for_response_end = std::chrono::system_clock::now() + response_timeout;
+    auto transmission_duration = std::chrono::microseconds((2'000'000 * bytes_to_send) / c.baudrate);
+    auto wait_for_response_end = std::chrono::system_clock::now() + response_timeout + transmission_duration;
 
     std::vector<std::byte> response;
     response.reserve(response_expected_length);
 
     while (response.size() < response_expected_length && (std::chrono::system_clock::now() < wait_for_response_end)) {
-        std::cout << ".";
         std::byte tmp;
         if (1U == c.read({&tmp, sizeof(tmp)})) {
             response.push_back(tmp);
+        } else {
+            if (verbose) {
+                ::putchar('.');
+            }
         }
         std::this_thread::sleep_for(100ms);
     }
+    if (verbose) {
+        ::putchar('\n');
+    }
+
+    if (response.size() != response_expected_length) {
+        return fail_with_reason(fmt::format("Invalid response (expected 4B, got {}B)", response.size()));
+    } else {
+        std::uint32_t received_checksum;
+        std::memcpy(&received_checksum, response.data(), sizeof(received_checksum));
+        if (received_checksum != checksum) {
+            return fail_with_reason(fmt::format("Invalid checksum (expected {:#x}, got {:#x})", checksum, received_checksum));
+        } else {
+            fmt::print(fg(fmt::color::aqua), "Test finished with result: ");
+            fmt::print(fg(fmt::color::lime_green), "PASS\n");
+        }
+    }
+    return 1;
 }
 
 int main(int argc, char** argv) {
-    std::cout << termcolor::yellow << "============ USART stress test application ============" << termcolor::reset << std::endl;
+    fmt::print(fg(fmt::color::lemon_chiffon), "======= USART stress test application =======\n");
     std::atexit(on_app_finished);
 
-    if (argc != 3) {
-        std::cerr << termcolor::red << "Invalid argument count" << termcolor::reset << std::endl;
-        display_help();
+    argparse::ArgumentParser program("usart_stress_test", APP_VERSION);
+
+    program.add_argument("port").required().help("serial port to connect to");
+    program.add_argument("-b", "--baudrate").help("serial port baudrate").default_value(115200).scan<'i', int>();
+    program.add_argument("-c", "--count").help("number of test iterations to perform").default_value(5).scan<'i', int>();
+    program.add_argument("-s", "--size").help("test payload size in bytes").default_value(10 * 1024).scan<'i', int>();
+    program.add_argument("--verbose").help("increase output verbosity").default_value(false).implicit_value(true);
+
+    int         size     = 0;
+    int         count    = 0;
+    int         baudrate = 0;
+    std::string port;
+
+    try {
+        program.parse_args(argc, argv);
+        port     = program.get<std::string>("port");
+        baudrate = program.get<int>("--baudrate");
+        size     = program.get<int>("--size");
+        count    = program.get<int>("--count");
+        verbose  = program.get<bool>("--verbose");
+    } catch (const std::exception& e) {
+        auto msg = fmt::format(fg(fmt::color::red), "Exception during parsing user input: {}\n", e.what());
+        std::cerr << msg << program << std::endl;
         return -1;
     }
 
-    auto baudrate = parse_baudrate(argv[2]);
-    if (0 == baudrate) {
-        std::cerr << "Invalid baudrate\n";
+    if (size <= 0) {
+        auto msg = fmt::format(fg(fmt::color::red), "Invalid payload size: {}\n", size);
+        std::cerr << msg << program << std::endl;
         return -2;
     }
 
-    std::cout << "Opening serial port: " << argv[1] << " with baudrate: " << baudrate << '\n';
-    // Use second argument as a serial port name
-    SerialConnection connection(argv[1], baudrate);
-    if (!connection.open()) {
-        std::cout << termcolor::red << "Opening serial port: " << connection.port << " failed - make sure that no other application uses this serial port" << termcolor::reset << std::endl;
-        return -3;
+    if (count <= 0) {
+        auto msg = fmt::format(fg(fmt::color::red), "Invalid test count: {}\n", count);
+        std::cerr << msg << program << std::endl;
+        return -2;
     }
 
-    std::cout << "connection opened\n";
+    if (baudrate <= 0) {
+        auto msg = fmt::format(fg(fmt::color::red), "Invalid baudrate: {}\n", baudrate);
+        std::cerr << msg << program << std::endl;
+        return -2;
+    }
 
 
-    perform_test(connection, 100);
-    perform_test(connection, 100);
-    perform_test(connection, 100);
+    if (verbose) {
+        fmt::print("Opening serial port: {} with baudrate: {}\n", port, baudrate);
+        fmt::print("{} tests to perform with {} bytes payload\n", count, size);
+    }
 
+
+    SerialConnection connection(port, baudrate);
+
+    if (!connection.open()) {
+        auto msg = fmt::format(fg(fmt::color::red), "Opening serial port: {} failed - make sure that no other application uses this serial port", connection.port);
+        std::cerr << msg << std::endl;
+        return -3;
+    }
+    if (verbose) {
+        fmt::print("connection opened\n");
+    }
+
+
+    auto successes = 0;
+    for (auto i = 0; i != count; ++i) {
+        fmt::print(fg(fmt::color::aqua), "Test {}/{}:\n", i + 1, count);
+        successes += perform_test(connection, size);
+    }
+
+    if (count == successes) {
+        fmt::print(fg(fmt::color::lime_green), "\n\t\tALL TESTS PASSED\n");
+    } else {
+        fmt::print(fg(fmt::color::indian_red), "\n\t\t{}/{} TESTS FAILED\n", count - successes, count);
+    }
 
     return 0;
 }
